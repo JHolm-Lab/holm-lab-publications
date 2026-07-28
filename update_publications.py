922a82b79b08ad6618efd8b21709d0e0b16014ce
Courtney Broedlow <44091352+CBroedlow@users.noreply.github.com>
2026-07-28 16:20:03 -0400

    Update update_publications.py

--- update_publications.py
+++ update_publications.py
@@ -15,7 +15,7 @@ from urllib.parse import quote
 
 import requests
 
-ROOT = Path(__file__).resolve().parents
+ROOT = Path(__file__).resolve().parent
 CONFIG_PATH = ROOT / "config.json"
 OUTPUT_PATH = ROOT / "publications.json"
 REVIEW_PATH = ROOT / "publication-review.json"
